b59e820bb4feb54a7bc0cdac13c6809e5c78ddd0
mfkucuk <mfkucuk555@gmail.com>
2021-04-21 14:29:33 +0300

    sub-classes of journey

---
+++ CS102 Project Coding.zip/Journey.zip/src/FutureJourney.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+/**
+ * A subclass of journey for future journeys of the user.
+ */
+public class FutureJourney extends Journey {
+    
+    // Constructor
+    public FutureJourney(int journeyID, String location, String description, Date startDate, Date endDate) {
+        super(journeyID, location, description, startDate, endDate);
+    }
+
+    /**
+     * Converts future journey to a current one.
+     */
+    public CurrentJourney convertToCurrentJourney() {
+        return new CurrentJourney(getJourneyID(), getLocation(), getDescription(), getStartDate(), getEndDate());
+    }
+
+}
